59febd442d63d435120d1415a226e854d3834972
Samuel <samuelreyesiglesias@gmail.com>
2023-11-14 11:06:45 -0600

    hola mundo Golang

--- 01_hola/hola.go
+++ 01_hola/hola.go
@@ -1,7 +1,14 @@
 package main
 
 import "fmt"
+//names fmt is the name of the package that contains the function Println
+//include the package fmt in the program
 
 func main() {
-	fmt.Println("Hola, Primer Hola Mundo en GoLang ")
+	mostrarHolaMundo();
 }
+//funcion para mostrar hola mundo
+func mostrarHolaMundo(){
+	fmt.Println("Hola Mundo en GoLang");
+}
+//execute in terminal: go run hola.go
